25f38e897753f170139b26f758897657054c2b0a
JosephPreciious <josephprecious419@gmail.com>
2025-01-09 17:35:34 +0100

    this program request usersto input their  names and printed it out calling their last name first then the full name

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -1,9 +1,17 @@
 using System;
 
-class Program
+internal class Program
 {
-    static void Main(string[] args)
+    private static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.WriteLine("Hello Welcome. Can you give us your name ");
+
+        Console.WriteLine("what is your first Name?");
+        string first_name = Console.ReadLine();
+
+        Console.WriteLine("what is your last name?");
+        string last_name = Console.ReadLine();
+
+        Console.WriteLine(value: $"Your name is {first_name}, {last_name} {first_name}");
     }
 }
