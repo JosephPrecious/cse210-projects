cf75cc3d2e76f47b20222dcbfaa824e1ab1c91ca
JosephPreciious <josephprecious419@gmail.com>
2025-01-10 23:05:00 +0100

    you if ,else if and else statement to get individuals grade and determine if dey pass or not

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,55 @@
 using System;
-
-class Program
+using System.Diagnostics;
+using System.IO.Compression;
+using System.Threading.Tasks.Dataflow;
+internal class Program
 {
-    static void Main(string[] args)
+    private static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+
+        Console.Write("What is your grade? %");
+        string gradeAnswer = Console.ReadLine();
+        int grade = int.Parse(gradeAnswer);
+
+        string gradeletter = "";
+
+        if (grade >= 90)
+        {
+            gradeletter = "A";
+        }
+
+        else if (grade >= 80)
+        {
+              gradeletter = "B";
+        }
+
+        else if (grade >= 70)
+        {
+             gradeletter = "C";
+        }
+
+        else if (grade >= 60)
+        {
+              gradeletter = "D";
+        }
+
+        else 
+        {
+             gradeletter = "F";
+        }
+
+        Console.WriteLine($"ypur grade is {gradeletter}");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("your passed your class");
+
+        }
+
+        else
+        {
+            Console.WriteLine("you faaid the course try again nest sementer");
+        }
     }
 }
