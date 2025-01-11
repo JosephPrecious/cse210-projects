1356de648f072860cb01e081628a03a2fa7d14b6
JosephPreciious <josephprecious419@gmail.com>
2025-01-11 16:08:43 +0100

    This is a Guess My Number game the computer picks a magic number, and then the user tries to guess it. After each guess, the computer tells the user to guess "higher" or "lower" until they guess the magic number.

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,41 @@
 using System;
 
-class Program
+internal class Program
 {
-    static void Main(string[] args)
+    private static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        //the first part of the project 
+        /*Console.Write("what is the magic number? ");
+        int number1 = int.Parse(Console.ReadLine());*/
+
+        Random randomGenerator = new Random();
+        int number1 = randomGenerator.Next(1, 100);//this will genarate a ramdom number number that i don't know 
+
+        int guess1 = 0;
+
+        while (number1 != guess1)
+        {
+            Console.Write("what is your guess? ");
+            int guess2 = int.Parse(Console.ReadLine());
+
+
+            if (guess2 < number1)
+            {
+                Console.WriteLine("Higher");
+            }
+
+            else if (guess2 > number1)
+            {
+                Console.WriteLine("Lower");
+            }
+
+            else
+            {
+                Console.WriteLine("you guessed it Right!");
+            }
+
+        }
     }
 }
