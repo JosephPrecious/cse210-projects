95901fad9916b4c7b64f33839bdccf07261d0c41
JosephPreciious <josephprecious419@gmail.com>
2025-02-01 21:22:38 +0100

    Added Video and Comment classes to store YouTube video details and user comments. Implemented functionality to display video info along with comments

--- week04/YouTubeVideos/Video.cs
+++ week04/YouTubeVideos/Video.cs
@@ -3,24 +3,24 @@ public class Video
     public string Title { get; set; }
     public string Author { get; set; }
     public int Length { get; set; } // Length in seconds
-    private List<Comment> comments;
+    private List<Comment> _comments;
 
     public Video(string title, string author, int length)
     {
         Title = title;
         Author = author;
         Length = length;
-        comments = new List<Comment>();
+        _comments = new List<Comment>();
     }
 
     public void AddComment(Comment comment)
     {
-        comments.Add(comment);
+        _comments.Add(comment);
     }
 
     public int GetCommentCount()
     {
-        return comments.Count;
+        return _comments.Count;
     }
 
     public void DisplayVideoInfo()
@@ -30,7 +30,7 @@ public class Video
         Console.WriteLine($"Length: {Length} seconds");
         Console.WriteLine($"Number of Comments: {GetCommentCount()}");
         Console.WriteLine("Comments:");
-        foreach (var comment in comments)
+        foreach (var comment in _comments)
         {
             Console.WriteLine($"- {comment.Name}: {comment.Text}");
         }
